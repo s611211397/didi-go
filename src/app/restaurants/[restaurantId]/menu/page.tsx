87df8d8a9467b91a97aec76d753adae9e2134d60
s611211397 <s611211397@gmail.com>
2025-04-09 09:31:56 +0800

    為關閉按鈕新增 aria-label，以改善無障礙性

--- src/app/restaurants/[restaurantId]/menu/page.tsx
+++ src/app/restaurants/[restaurantId]/menu/page.tsx
@@ -171,6 +171,7 @@ const MenuItemForm: React.FC<MenuItemFormProps> = ({
           <button 
             onClick={onClose}
             className="text-[#767676] hover:text-[#484848] cursor-pointer mt-2"
+            aria-label="關閉"
           >
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
